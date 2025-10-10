54c1598eb47345059772985073fac12f63c486dc
anmitsu <mitsu.6200@gmail.com>
2025-10-11 08:20:40 +0900

    講義18を視聴しながら動かした内容をLesson18_memoブランチにpushしてみる

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -3,19 +3,35 @@ package raisetech.student.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+  private String privateName = "Nagaya Mitsuyo";
+  private String privateAge = "29";
+
   public static void main(String[] args) {
     // localhost:8080
     SpringApplication.run(Application.class, args);
   }
 
-  @GetMapping("/hello")
-  public String hello() {
-    return "Hello, World!!!!";
+  @GetMapping("/studentInfo")
+  public String getStudentInfo() {
+    return privateName + " " + privateAge + "歳";
   }
+
+  @PostMapping("/studentInfo")
+  public void setStudentInfo(String privateName,String privateAge){
+    this.privateName = privateName;
+    this.privateAge = privateAge;
+  }
+
+  @PostMapping("/updateStudentName")
+  public void  updateStudentName(String privateName){
+    this.privateName = privateName;
+  }
+
 }
