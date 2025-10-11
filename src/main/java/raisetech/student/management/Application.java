3c390bf056ff375acc1ce0bf3f6dd36da6fc40c1
anmitsu <mitsu.6200@gmail.com>
2025-10-11 09:16:37 +0900

    一人ずつPostしてMapに追加していくことに成功

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,17 +1,23 @@
 package raisetech.student.management;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-  private String privateName = "Nagaya Mitsuyo";
-  private String privateAge = "29";
+  //課題
+  //複数の情報を登録したらどうなる？
+  //Mapの中の一部の情報を更新したい場合（Keyに紐づく受講生の情報をアップデートしたい）として、Postするとどうなる？
+  private Map<String,String> student = new HashMap<>();
+
 
   public static void main(String[] args) {
     // localhost:8080
@@ -20,15 +26,15 @@ public class Application {
 
   @GetMapping("/studentInfo")
   public String getStudentInfo() {
-    return privateName + " " + privateAge + "歳";
+    return student.toString();
   }
 
   @PostMapping("/studentInfo")
-  public void setStudentInfo(String privateName,String privateAge){
-    this.privateName = privateName;
-    this.privateAge = privateAge;
+  public void setStudentInfo(@RequestParam String name, @RequestParam String age){
+    student.put(name,age);
   }
 
+  //以下いったん残している
   @PostMapping("/updateStudentName")
   public void  updateStudentName(String privateName){
     this.privateName = privateName;
