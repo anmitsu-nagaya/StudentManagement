4aeb6cee15b692700a9cf0979de679cb127ff77c
anmitsu <mitsu.6200@gmail.com>
2025-10-16 17:45:35 +0900

    【講義内容】登録ボタンを押下して受講生情報一覧が表示されるところまで完了、この後課題

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -6,7 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.student.management.controller.converter.StudentConverter;
 import raisetech.student.management.data.Student;
@@ -26,13 +30,12 @@ public class StudentController {
     this.converter = converter;
   }
 
+
   @GetMapping("/students")
   public String getStudentList(Model model) {
     List<Student> students = service.searchStudentList();
     List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
-
     model.addAttribute("studentList",converter.convertStudentDetails(students,studentsCourses));
-
     return "studentList";
   }
 
@@ -40,4 +43,24 @@ public class StudentController {
   public List<StudentsCourses> getStudentsCourseList() {
     return service.searchStudentsCourseList();
   }
+
+  @GetMapping("/new-student")
+  public String newStudent(Model model){
+    model.addAttribute("studentDetail", new StudentDetail());//th:object="${studentDetail}に渡る
+    return "registerStudent"; //registerStudent.htmlのこと
+  }
+
+  @PostMapping("/register-student")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result){
+    if(result.hasErrors()){
+      return "registerStudent";
+    }
+
+    //System.out.println(studentDetail.getStudent().getStudentFullName() + "さんが新規受講生として登録されました。");
+    //新規受講生情報を登録する処理を実装する。
+    //コース情報も一緒に登録できるように実装する。コースは単体で良い。
+
+    return "redirect:/students";
+  }
+
 }
