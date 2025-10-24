efca2561b97d78f327206ac4e787ec2c755cc8a0
anmitsu <mitsu.6200@gmail.com>
2025-10-24 15:26:13 +0900

    バリデーションを追加

--- src/main/java/raisetech/student/management/data/Student.java
+++ src/main/java/raisetech/student/management/data/Student.java
@@ -1,5 +1,9 @@
 package raisetech.student.management.data;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,35 +21,51 @@ public class Student {
   /**
    * 学生のフルネーム。
    */
+  @NotBlank
+  @Size(max = 100)
   private String studentFullName;
   /**
    * 学生のフリガナ（カタカナ）。
    */
+  @NotBlank
+  @Size(max = 100)
   private String studentFurigana;
+  /**
+   * 学生のニックネーム。
+   */
+  @Size(max = 50)
   private String studentNickname;
   /**
    * メールアドレス。
    */
+  @NotBlank
+  @Email
+  @Size(max = 254)
   private String email;
   /**
    * 地域（都道府県）。
    */
+  @Size(max = 10)
   private String prefecture;
   /**
    * 地域（市区町村）。
    */
+  @Size(max = 10)
   private String city;
   /**
    * 年齢。
    */
-  private int age;
+  @Min(1)
+  private Integer age;
   /**
    * 性別。
    */
+  @Size(max = 20)
   private String gender;
   /**
    * 備考欄。
    */
+  @Size(max = 500)
   private String studentRemark;
   /**
    * 論理削除フラグ。
