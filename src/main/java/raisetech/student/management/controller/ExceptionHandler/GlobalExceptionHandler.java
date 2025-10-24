b4989f4f5fc0ab2130f7427f1d0554f74eb7f74e
anmitsu <mitsu.6200@gmail.com>
2025-10-24 18:19:52 +0900

    importを別に分けた

--- src/main/java/raisetech/student/management/controller/ExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/raisetech/student/management/controller/ExceptionHandler/GlobalExceptionHandler.java
@@ -3,12 +3,13 @@ package raisetech.student.management.controller.ExceptionHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import raisetech.student.management.Exception.TestException;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
-  @org.springframework.web.bind.annotation.ExceptionHandler(TestException.class)
+  @ExceptionHandler(TestException.class)
   public ResponseEntity<String> handleTestException(TestException ex) {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
   }
