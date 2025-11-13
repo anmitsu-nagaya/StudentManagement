ac6c2c9a8ebb297e9c1e1376c916ac7d634c5bf2
anmitsu <mitsu.6200@gmail.com>
2025-11-13 21:19:21 +0900

    単一検索がなくなったため変更

--- src/test/java/raisetech/student/management/controller/StudentControllerTest.java
+++ src/test/java/raisetech/student/management/controller/StudentControllerTest.java
@@ -188,10 +188,10 @@ class StudentControllerTest {
   void リクエストのクエリパラメータに不正な値が渡されたときにエラーレスポンスが返ること_1()
       throws Exception {
     String id = "ID";
-    mockMvc.perform(MockMvcRequestBuilders.get("/student/{studentId}", id))
+    mockMvc.perform(MockMvcRequestBuilders.get("/students/filter?studentId=" + id))
         .andExpect(status().is4xxClientError())
         .andExpect(content().string(
-            "showStudentDetail.id: UUIDの形式が正しくありません。"));
+            "getFilterStudentList.studentId: UUIDの形式が正しくありません。"));
     String name = "a".repeat(101);
     mockMvc.perform(MockMvcRequestBuilders.get("/students/filter?studentFullName=" + name))
         .andExpect(status().is4xxClientError())
